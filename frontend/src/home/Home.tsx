8cdd1df1d25bbd93c4b9d77f17fc542508f57a22
Jamshid <jroid8@tutanota.com>
2025-02-11 21:39:01 +0330

    add react-router

---
+++ frontend/src/home/Home.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+	return <>
+		<h1>Play Board Game Classics with your Friends</h1>
+	</>
+}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { BrowserRouter, Routes } from "react-router";
+import { Route } from "react-router";
+import Home from "./home/Home";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
-)
+);
