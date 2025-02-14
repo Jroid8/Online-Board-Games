80185725eea9c418362724aea509d2ef27c5679c
Jamshid <jroid8@tutanota.com>
2025-02-14 17:02:43 +0330

    feat(front): pastel SearchSortBar

--- frontend/src/home/SearchSortBar.tsx
+++ frontend/src/home/SearchSortBar.tsx
@@ -1,47 +1,50 @@
-import './SearchSortBar.css';
+import "./SearchSortBar.css";
 import { useEffect, useState } from "react";
 import GameInfo from "./GameInfo";
+import randPastelColor from "../common/randPastelColor";
 
 const sortByToOrderName: { [key: string]: string[] } = {
-  name: ["A-Z", "Z-A"],
-  player: ["Descending", "Ascending"],
+	name: ["A-Z", "Z-A"],
+	player: ["Descending", "Ascending"],
 };
 
 export default function SearchSortBar({
-  gameList,
-  setGameList,
+	gameList,
+	setGameList,
 }: {
-	gameList: GameInfo[],
-  setGameList: React.Dispatch<React.SetStateAction<GameInfo[]>>;
+	gameList: GameInfo[];
+	setGameList: React.Dispatch<React.SetStateAction<GameInfo[]>>;
 }) {
-  const [sortBy, setSortBy] = useState("name");
-  const [order, setOrder] = useState("nomral");
-  const [orderNames, setOrderNames] = useState(sortByToOrderName.name);
+	const [sortBy, setSortBy] = useState("name");
+	const [order, setOrder] = useState("nomral");
+	const [orderNames, setOrderNames] = useState(sortByToOrderName.name);
 
-  useEffect(() => {
-    setOrderNames(sortByToOrderName[sortBy]);
-  }, [sortBy]);
+	useEffect(() => {
+		setOrderNames(sortByToOrderName[sortBy]);
+	}, [sortBy]);
 
-  return (
-    <div className="top-bar">
-      <input type="text" />
-      <span>Order by:</span>
-      <select
-        name="sort"
-        value={sortBy}
-        onChange={(e) => setSortBy(e.target.value)}
-      >
-        <option value="name">Name</option>
-        <option value="player">Player Count</option>
-      </select>
-      <select
-        name="order"
-        value={order}
-        onChange={(e) => setOrder(e.target.value)}
-      >
-        <option value="normal">{orderNames[0]}</option>
-        <option value="reverse">{orderNames[1]}</option>
-      </select>
-    </div>
-  );
+	return (
+		<div className="top-bar">
+			<input type="text" style={{ backgroundColor: randPastelColor() }} />
+			<span>Order by:</span>
+			<select
+				name="sort"
+				value={sortBy}
+				onChange={(e) => setSortBy(e.target.value)}
+				style={{ backgroundColor: randPastelColor() }}
+			>
+				<option value="name">Name</option>
+				<option value="player">Player Count</option>
+			</select>
+			<select
+				name="order"
+				value={order}
+				onChange={(e) => setOrder(e.target.value)}
+				style={{ backgroundColor: randPastelColor() }}
+			>
+				<option value="normal">{orderNames[0]}</option>
+				<option value="reverse">{orderNames[1]}</option>
+			</select>
+		</div>
+	);
 }
