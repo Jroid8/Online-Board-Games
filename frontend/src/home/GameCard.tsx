965ef910014fcc104b4090b65bd4b10cc0bb77c8
Jamshid <jroid8@tutanota.com>
2025-02-19 14:21:59 +0330

    feat(front): Make game cards links

--- frontend/src/home/GameCard.tsx
+++ frontend/src/home/GameCard.tsx
@@ -2,30 +2,32 @@ import PastelFloatBtn from "../common/PastelFloatBtn.tsx";
 import GameInfo from "./GameInfo";
 
 export default function GameCard({ info }: { info: GameInfo }) {
-  return (
-    <PastelFloatBtn
-      css={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        margin: "1.2vmin",
-        padding: "0.7em",
-      }}
-    >
-      <img
-        css={{
-          border: "1px black solid",
-          width: "12rem",
-          height: "12rem",
-        }}
-        src={info.thumbnailURL}
-        alt={info.name + " image"}
-      />
-      <span>{info.name}</span>
-      <span css={{ fontSize: "0.8rem" }} className="online">
-        currently playing: {info.playersOnline.toLocaleString()}
-      </span>
-    </PastelFloatBtn>
-  );
+	return (
+		<PastelFloatBtn
+			as="a"
+			href={"/games/" + info.id}
+			css={{
+				display: "flex",
+				flexDirection: "column",
+				justifyContent: "center",
+				alignItems: "center",
+				margin: "1.2vmin",
+				padding: "0.7em",
+			}}
+		>
+			<img
+				css={{
+					border: "1px black solid",
+					width: "12rem",
+					height: "12rem",
+				}}
+				src={info.thumbnailURL}
+				alt={info.name + " image"}
+			/>
+			<span>{info.name}</span>
+			<span css={{ fontSize: "0.8rem" }} className="online">
+				currently playing: {info.playersOnline.toLocaleString()}
+			</span>
+		</PastelFloatBtn>
+	);
 }
