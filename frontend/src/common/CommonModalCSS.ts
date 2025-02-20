4be912bd4386b43bf7171e2aab35a3b12f8e1b2e
Jamshid <jroid8@tutanota.com>
2025-02-20 22:13:00 +0330

    feat(front): CommonModalCSS's shadow color in one variable

--- frontend/src/common/CommonModalCSS.ts
+++ frontend/src/common/CommonModalCSS.ts
@@ -1,6 +1,8 @@
+const shadow = "hsl(0 0% 14%)";
+
 export default {
   borderRadius: "2ch",
-  padding: "3ch",
-  border: "2px black solid",
-  boxShadow: "6px 9px black",
+  padding: "3.7ch",
+  border: "2px solid " + shadow,
+  boxShadow: "6px 9px " + shadow,
 };
