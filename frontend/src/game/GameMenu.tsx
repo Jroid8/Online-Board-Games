ce748212472d69d92effa5555ab531f843c475c3
Jamshid <jroid8@tutanota.com>
2025-02-20 08:10:08 +0330

    feat(front): better button spacing for GameMenu

--- frontend/src/game/GameMenu.tsx
+++ frontend/src/game/GameMenu.tsx
@@ -4,35 +4,44 @@ import Cookies from "js-cookie";
 import PastelFloatBtn from "../common/PastelFloatBtn";
 
 export default function GameMenu({ info }: { info: GameInfo }) {
-	return (
-		<div
-			css={{
-				backgroundColor: randPastelColor().toString(),
-				borderRadius: "2vmin",
-				padding: "5vmin",
-				display: "flex",
-				gap: "4vw",
-			}}
-		>
-			<img
-				src={"/thumbnails/" + info.id}
-				css={{
-					height: "33vmin",
-					width: "33vmin",
-				}}
-			/>
-			<div css={{ display: "flex", flexDirection: "column" }}>
-				<h1>Play {info.name}</h1>
-				<PastelFloatBtn as="button">Random Match</PastelFloatBtn>
-				<PastelFloatBtn as="button">Create Invite Link</PastelFloatBtn>
-				<PastelFloatBtn
-					as="button"
-					disabled={!Cookies.get("session")}
-					title={Cookies.get("session") ? undefined : "Requires Login"}
-				>
-					Invite Friend
-				</PastelFloatBtn>
-			</div>
-		</div>
-	);
+  return (
+    <div
+      css={{
+        backgroundColor: randPastelColor().toString(),
+        borderRadius: "2vmin",
+        padding: "5vmin",
+        display: "flex",
+        gap: "4vw",
+      }}
+    >
+      <img
+        src={"/thumbnails/" + info.id}
+        css={{
+          height: "33vmin",
+          width: "33vmin",
+        }}
+      />
+      <div>
+        <h1>Play {info.name}</h1>
+        <div
+          css={{
+						height: "50%",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
+          }}
+        >
+          <PastelFloatBtn as="button">Random Match</PastelFloatBtn>
+          <PastelFloatBtn as="button">Create Invite Link</PastelFloatBtn>
+          <PastelFloatBtn
+            as="button"
+            disabled={!Cookies.get("session")}
+            title={Cookies.get("session") ? undefined : "Requires Login"}
+          >
+            Invite Friend
+          </PastelFloatBtn>
+        </div>
+      </div>
+    </div>
+  );
 }
