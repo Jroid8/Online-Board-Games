0f22f165c73bbf085ab70a3fdd774bfc8a295298
Jamshid <jroid8@tutanota.com>
2025-02-23 00:43:54 +0330

    feat(back): Better mass join handling

--- backend/src/game.ts
+++ backend/src/game.ts
@@ -29,7 +29,16 @@ export abstract class GameRoom implements Room {
   }
 
   addPlayers(players: Player[]) {
-    for (const p of players) this.addPlayer(p);
+    const presentPlayersMsg = Buffer.alloc(2 + players.length * 4);
+    presentPlayersMsg.writeUInt8(230);
+    presentPlayersMsg.writeUInt8(players.length, 1);
+    for (let i = 0; i < players.length; i++)
+      presentPlayersMsg.writeUInt32BE(players[i].id, i * 4 + 2);
+		for (const p of players) {
+			p.room = this;
+			p.ws!.send(presentPlayersMsg);
+		}
+		this.players = players;
   }
 
   onDisconnect(player: Player, hub: Room) {
