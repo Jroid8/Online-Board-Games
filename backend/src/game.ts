5719e9011cf1d4a3fbbe943c49049753d39b126c
Jamshid <jroid8@tutanota.com>
2025-02-23 00:20:42 +0330

    feat(back): Inform when players join to present players

--- backend/src/game.ts
+++ backend/src/game.ts
@@ -12,6 +12,12 @@ export abstract class GameRoom implements Room {
   players: Player[] = [];
 
   addPlayer(player: Player) {
+    // joining doesn't happen when the player is absent
+    // so using player.ws! should be safe
+    const joinBroadcastMsg = Buffer.alloc(5);
+    joinBroadcastMsg.writeUInt8(231);
+    joinBroadcastMsg.writeUInt32BE(player.id, 1);
+    for (const p of this.players) p.ws!.send(joinBroadcastMsg);
     this.players.push(player);
     player.room = this;
     // joining doesn't happen when the player is absent, so this should be safe
@@ -25,7 +31,7 @@ export abstract class GameRoom implements Room {
   onDisconnect(player: Player, hub: Room) {
     setTimeout(
       () => {
-				player.room = hub;
+        player.room = hub;
         for (let i = 0; i < this.players.length; i++)
           if (this.players[i].id == player.id) this.players.splice(i, 1);
       },
