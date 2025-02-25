734412a66892eb7282d451d30d70592cca3869fe
Jamshid <jroid8@tutanota.com>
2025-02-25 20:54:52 +0330

    refactor(back): reduce GameRoom logic redundancy

--- backend/src/game-room.ts
+++ backend/src/game-room.ts
@@ -5,135 +5,127 @@ import Room from "./room";
 export const gameMsgCodes = Object.freeze({
   joinedRoom: 0xc0,
   otherPlayerJoinedRoom: 0xc1,
-  otherPlayerDisconnected: 0xc1,
-  gameStart: 0xc2,
+  otherPlayerDisconnected: 0xc3,
+  gameStarted: 0xc2,
   gameConcluded: 0xc8,
-  stateUpdate: 0xd0,
 });
 
 // Classes extending this MUST HAVE GameInfo fields as static fields
 // While typescript will prevent classes which don't do this from being included in gameList
 // Problems will clearly arise once a sub class which doesn't abide by this rule is constructed outside of gameList
 export abstract class GameRoom implements Room {
-  players: Player[] = [];
-  forfeitTimeouts: Map<number, NodeJS.Timeout> = new Map();
+  protected players: Player[] = [];
+  protected playerIndex: Map<number, number> = new Map();
+  private forfeitTimeouts: Map<number, NodeJS.Timeout> = new Map();
+
+  protected broadcastMessage(message: Buffer, excludePlayer?: Player) {
+    for (const p of this.players) {
+      if (p.ws && p.id !== excludePlayer?.id) {
+        p.ws.send(message);
+      }
+    }
+  }
 
   private staticFields(): GameInfo {
     return this.constructor as unknown as GameInfo;
   }
 
-  constructor(players?: Player[]) {
-    if (players) {
-      this.players = players;
-      const gameStarted = players.length == this.staticFields().playerCount;
-      const buffSize = 3 + (players.length - 1) * 4;
-      const gameInitMsg = gameStarted ? this.begin() : Buffer.alloc(0);
-      for (let i = 0; i < players.length; i++) {
-        const generalStateMsg = Buffer.alloc(buffSize);
-        generalStateMsg.writeUInt8(gameMsgCodes.joinedRoom);
-        generalStateMsg.writeUInt8(gameStarted ? 1 : 0, 1);
-        generalStateMsg.writeUInt8(players.length - 1, 2);
-        let j = 0;
-        for (const p of players) {
-          if (p.id == players[i].id) continue;
-          generalStateMsg.writeUInt32BE(p.id, j * 4 + 3);
-          j++;
-        }
-        players[i].room = this;
-        // joining doesn't happen when the player is absent
-        // so using player.ws! should be safe
-        players[i].ws!.send(Buffer.concat([generalStateMsg, gameInitMsg]));
-      }
+  protected serializePlayerIDs(excludePlayer?: Player): Buffer {
+    const exNum = excludePlayer ? 1 : 0;
+    const buff = Buffer.alloc(1 + (this.players.length - exNum) * 4);
+    let j = 0;
+    buff.writeUInt8(this.players.length - exNum);
+    for (const p of this.players) {
+      if (p.id === excludePlayer?.id) continue;
+      buff.writeUInt32BE(p.id, j * 4 + 3);
+      j++;
     }
+    return buff;
   }
 
-  informPlayerJoin(player: Player) {
+  public isGameStarted(): boolean {
+    return this.players.length >= this.staticFields().playerCount;
+  }
+
+  private sendFullJoinMsg(player: Player) {
+    player.ws!.send(
+      Buffer.concat([
+        Buffer.from([gameMsgCodes.joinedRoom, this.isGameStarted() ? 1 : 0]),
+        this.serializePlayerIDs(player),
+        this.isGameStarted() ? this.serializeState() : Buffer.alloc(0),
+      ]),
+    );
+  }
+
+  private informPlayerJoin(player: Player) {
     const joinBroadcastMsg = Buffer.alloc(5);
     joinBroadcastMsg.writeUInt8(gameMsgCodes.otherPlayerJoinedRoom);
     joinBroadcastMsg.writeUInt32BE(player.id, 1);
-    for (const p of this.players)
-      if (p.ws && p.id != player.id) p.ws.send(joinBroadcastMsg);
+    this.broadcastMessage(joinBroadcastMsg, player);
   }
 
-  getPlayerIndex(player: Player): number | null {
-    const playerCount = this.staticFields().playerCount;
-    for (let i = 0; i < this.players.length && i < playerCount; i++)
-      if (this.players[i].id == player.id) return i;
-    return null;
+  private initPlayerIndex() {
+    for (let i = 0; i < this.staticFields().playerCount; i++)
+      this.playerIndex.set(this.players[i].id, i);
   }
 
-  register(player: Player) {
-    const playerCount = this.staticFields().playerCount;
-    this.informPlayerJoin(player);
-    const stateMsg = Buffer.alloc(2 + this.players.length * 4);
-    stateMsg.writeUInt8(gameMsgCodes.joinedRoom);
-    stateMsg.writeUInt8(this.players.length, 1);
-    for (let i = 0; i < this.players.length; i++)
-      stateMsg.writeUInt32BE(this.players[i].id, i * 4 + 2);
-    // add serialized game state to message
-    player.ws!.send(stateMsg);
-    this.players.push(player);
-    player.room = this;
-    if (this.players.length == playerCount) {
-      const initMsg = this.begin();
-      for (let i = 0; i < this.players.length; i++)
-        this.players[i].ws!.send(
-          Buffer.concat([Buffer.from([gameMsgCodes.gameStart]), initMsg]),
-        );
-    } else if (this.players.length > playerCount) {
-      player.ws!.send(
-        Buffer.concat([
-          Buffer.from([gameMsgCodes.stateUpdate]),
-          this.serializeState(),
-        ]),
+  private startGameIfCan() {
+    if (this.players.length == this.staticFields().playerCount) {
+      this.initPlayerIndex();
+      this.broadcastMessage(
+        Buffer.concat([Buffer.from([gameMsgCodes.gameStarted]), this.begin()]),
       );
     }
   }
 
-  onDisconnect(player: Player) {
-    const playerCount = this.staticFields().playerCount;
+  protected isSpectator(player: Player) {
+    return this.playerIndex.get(player.id) !== undefined;
+  }
+
+  public constructor(players: Player[]) {
+    this.players = players;
+    for (const p of this.players) {
+      p.room = this;
+      this.sendFullJoinMsg(p);
+    }
+    if (this.isGameStarted()) this.initPlayerIndex();
+  }
+
+  public register(player: Player) {
+    this.informPlayerJoin(player);
+    this.sendFullJoinMsg(player);
+    this.players.push(player);
+    player.room = this;
+    this.startGameIfCan();
+  }
+
+  public onDisconnect(player: Player) {
     let disconnectMsg = Buffer.alloc(5);
     disconnectMsg.writeUInt8(gameMsgCodes.otherPlayerDisconnected);
     disconnectMsg.writeUInt32BE(player.id, 1);
-    let notThisPlayer = this.players.filter((p) => p.id != player.id);
-    if (
-      this.players.length >= playerCount &&
-      this.getPlayerIndex(player) !== null
-    ) {
-      for (const p of notThisPlayer) if (p.ws) p.ws.send(disconnectMsg);
+    if (this.isGameStarted() && this.playerIndex.get(player.id) !== undefined) {
       this.forfeitTimeouts.set(
         player.id,
         setTimeout(
           () => {
-            for (const p of notThisPlayer) {
-              if (p.ws) p.ws.send(Buffer.from([gameMsgCodes.gameConcluded]));
-              p.room = globalThis.hub;
-            }
+            this.broadcastMessage(Buffer.from([gameMsgCodes.gameConcluded]));
+            for (const p of this.players) p.room = globalThis.hub;
             this.players = [];
           },
           1000 * 60 * 2.5,
         ),
       );
     } else {
-      this.players = notThisPlayer;
+      this.players = this.players.filter((p) => p.id != player.id);
       player.room = globalThis.hub;
-      for (const p of this.players) p.ws!.send(disconnectMsg);
     }
+    this.broadcastMessage(disconnectMsg);
   }
 
-  onRejoin(player: Player): void {
+  public onRejoin(player: Player): void {
     clearTimeout(this.forfeitTimeouts.get(player.id));
     this.informPlayerJoin(player);
-    const generalStateMsg = Buffer.alloc(2 + (this.players.length + 1) * 4);
-    generalStateMsg.writeUInt8(gameMsgCodes.joinedRoom);
-    generalStateMsg.writeUInt8(this.players.length - 1, 1);
-    let j = 0;
-    for (const p of this.players) {
-      if (p.id == this.players[j].id) continue;
-      generalStateMsg.writeUInt32BE(p.id, j * 4 + 3);
-      j++;
-    }
-    player.ws!.send(Buffer.concat([generalStateMsg, this.serializeState()]));
+    this.sendFullJoinMsg(player);
   }
 
   abstract onMessage(player: Player, message: Buffer): void;
