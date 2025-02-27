aea2391826a36ce4caf5a0511c7b69f7f80463df
Jamshid <jroid8@tutanota.com>
2025-02-28 01:04:33 +0330

    fix(front): incorrect offsets in tic-tac-toe listener

--- frontend/src/game/TicTacToe/listener.ts
+++ frontend/src/game/TicTacToe/listener.ts
@@ -1,9 +1,18 @@
 import { useStateStore } from "../ClientState";
-import { CurrentState, markCell } from "./states";
+import { Cell, CurrentState, markCell } from "./states";
 
 export default function listener(msg: DataView<ArrayBuffer>) {
 	const state = useStateStore.getState() as CurrentState;
-	if (msg.buffer.byteLength !== 2 && msg.getUint8(0) !== 1) return;
-	markCell(state, useStateStore.setState, msg.getUint8(1));
-	useStateStore.setState({ myTurn: true });
+	if (msg.buffer.byteLength < 2) return;
+	if (msg.getUint8(0) === 1) {
+		markCell(
+			state,
+			useStateStore.setState,
+			msg.getUint8(5),
+			msg.getUint32(1) === state.xPlayer ? Cell.X : Cell.O,
+		);
+	} else if (msg.getUint8(0) === 0xd0 && msg.buffer.byteLength === 5) {
+		const id = msg.getUint32(1);
+		useStateStore.setState({ turn: id, myTurn: id === state.user.id });
+	}
 }
