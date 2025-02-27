f09f8ae26d5918602273be8012bf7e0b7f9a405c
Jamshid <jroid8@tutanota.com>
2025-02-27 09:13:44 +0330

    feat(front): better visuals for TicTacToe

--- frontend/src/game/TicTacToe/component.tsx
+++ frontend/src/game/TicTacToe/component.tsx
@@ -1,49 +1,63 @@
 import { useStateStore } from "../ClientState";
 import { Cell } from "./states";
+import xSVG from "./xmark.svg";
+import oSVG from "./omark.svg";
+import randPastelColor from "../../utils/RandPastelColor";
 
 function Slot({ value, onClick }: { value: Cell; onClick: () => void }) {
-	let content = " ";
+	let img = "";
 	switch (value) {
 		case Cell.X:
-			content = "X";
+			img = xSVG;
 			break;
 		case Cell.O:
-			content = "O";
+			img = oSVG;
 			break;
 	}
 	return (
 		<button
 			css={{
-				display: "inline-flex",
-				alignItems: "center",
-				justifyContent: "center",
-				padding: 0,
 				backgroundColor: "white",
-				border: "0.1ch",
-				outline: "none",
-				borderRadius: "1rem",
-				fontWeight: "bold",
-				fontSize: "10vmin",
+				borderRadius: "2vmin",
+				border: "none",
+				boxShadow: "0 0 25px #aaa"
 			}}
 			onClick={onClick}
 		>
-			{content}
+			<img
+				src={img}
+				css={{
+					height: "70%",
+					display: "block",
+					marginLeft: "auto",
+					marginRight: "auto",
+				}}
+			/>
 		</button>
 	);
 }
 
 export default function TicTacToe() {
-	const state = useStateStore();
+	// const state = useStateStore();
 	return (
 		<div
 			css={{
 				display: "grid",
-				gridTemplateRows: "repeat(3, 1fr)",
-				gridTemplateColumns: "repeat(3, 1fr)",
+				gridTemplateRows: "repeat(3, 28vmin)",
+				gridTemplateColumns: "repeat(3, 28vmin)",
 				height: "100%",
+				gap: "3vmin",
 			}}
 		>
-			{}
+			<Slot value={1} onClick={() => {}} />
+			<Slot value={0} onClick={() => {}} />
+			<Slot value={1} onClick={() => {}} />
+			<Slot value={1} onClick={() => {}} />
+			<Slot value={2} onClick={() => {}} />
+			<Slot value={2} onClick={() => {}} />
+			<Slot value={2} onClick={() => {}} />
+			<Slot value={0} onClick={() => {}} />
+			<Slot value={0} onClick={() => {}} />
 		</div>
 	);
 }
