2f856e1b03cea94b02fbb885bfb5c11e1a6861bf
Jamshid <jroid8@tutanota.com>
2025-02-28 22:28:51 +0330

    feat(back): sign-in and login

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,10 +4,11 @@ import { Server as WebSocketServer } from "ws";
 import express from "express";
 import * as cookie from "cookie";
 import { Player } from "./player";
-import { randomBytes } from "node:crypto";
+import { randomBytes, createHash } from "node:crypto";
 import { IncomingHttpHeaders } from "node:http2";
 import Hub from "./hub";
 import log from "loglevel";
+import { neon } from "@neondatabase/serverless";
 
 // Global Command Codes:
 // - 0xFE: Message (the recipient depends on room)
@@ -16,6 +17,7 @@ import log from "loglevel";
 const app = express();
 const server = createServer(app);
 const wss = new WebSocketServer({ server, maxPayload: 256 });
+const sql = neon(process.env.DATABASE_URL!);
 
 const tokenPlayerMap: Map<string, number> = new Map();
 const playerForgetTimeouts: Map<number, NodeJS.Timeout> = new Map();
@@ -68,14 +70,28 @@ function handleChatMsg(sender: Player, msg: Buffer): boolean {
 	return true;
 }
 
-wss.on("headers", (headers, req) => {
+wss.on("headers", async (headers, req) => {
 	if (!req.headers.cookie) createGuestSession(headers, req.headers);
 	else {
 		let session = cookie.parse(req.headers.cookie).session;
-		if (session && tokenPlayerMap.get(session)) return;
-		// If exists on database load it into tokenPlayerMap and players
-		// else
-		createGuestSession(headers, req.headers);
+		if (session) {
+			if (tokenPlayerMap.get(session)) return;
+			let query = await sql(
+				`SELECT u.user_id, u.username FROM user_session AS us
+JOIN "user" AS u ON us.user_id=u.id WHERE tokenstr='${session}'`,
+			);
+			if (query.length > 0) {
+				const player: Player = {
+					id: query[0].user_id,
+					name: query[0].username,
+					isGuest: false,
+					room: hub,
+					ws: null
+				};
+				tokenPlayerMap.set(session, player.id);
+				globalThis.onlinePlayers.set(query[0].user_id, player)
+			}
+		} else createGuestSession(headers, req.headers);
 	}
 });
 
@@ -87,7 +103,9 @@ function sendUserInfo(player: Player) {
 
 wss.on("connection", (ws, req) => {
 	let token = cookie.parse(req.headers.cookie!).session!; // no cookies and lack of the session cookie would have been caught
-	let player = globalThis.onlinePlayers.get(tokenPlayerMap.get(token)!) as Player; // loading player data should have been handled in the headers event
+	let player = globalThis.onlinePlayers.get(
+		tokenPlayerMap.get(token)!,
+	) as Player; // loading player data should have been handled in the headers event
 
 	player.ws = ws;
 	if (playerForgetTimeouts.get(player.id)) {
@@ -120,6 +138,93 @@ wss.on("connection", (ws, req) => {
 		);
 		log.info(`<${player.id}> disconnected`);
 	});
+});
+
+async function createUserSession(
+	id: number,
+	name: string,
+): Promise<[string, Date]> {
+	const token = genSessionToken();
+	tokenPlayerMap.set(token, id);
+	globalThis.onlinePlayers.set(id, {
+		id,
+		ws: null,
+		name,
+		isGuest: false,
+		room: hub,
+	});
+	let expires = new Date(Date.now());
+	expires.setDate(expires.getDate() + 60);
+	await sql(
+		`INSERT INTO user_session (tokenstr, expiration, user_id)
+ VALUES ($1, $2, ${id})`,
+		[name, expires.toDateString().substring(4).replaceAll(" ", "-")],
+	);
+	return [token, expires];
+}
+
+app.post("/login", async (req, res) => {
+	if (typeof req.body != "string") {
+		res.status(400).send("Must send login information in plain text");
+		return;
+	}
+	let cred = req.body.split(":");
+	if (cred.length != 2) {
+		res.status(400).send("Must seperate username and password with a colon");
+		return;
+	}
+	let passwdHash = createHash("sha256").update(cred[1]).digest("base64");
+	try {
+		let userquery = await sql(
+			`SELECT id FROM "user" WHERE username='${cred[0]}' AND password_hash='${passwdHash}'`,
+		);
+		if (userquery.length > 0) {
+			let [token, expires] = await createUserSession(userquery[0].id, cred[0]);
+			res.cookie("session", token, { expires }).sendStatus(200);
+		} else {
+			res.sendStatus(401);
+		}
+	} catch (e) {
+		console.error(e);
+		res.sendStatus(500);
+	}
+});
+
+app.put("/signin", async (req, res) => {
+	if (typeof req.body != "string") {
+		res.status(400).send("Must send login information in plain text");
+		return;
+	}
+	let cred = req.body.split(":");
+	if (cred.length != 2) {
+		res.status(400).send("Must seperate username and password with a colon");
+		return;
+	}
+	try {
+		if (
+			(
+				await sql(`SELECT 1 FROM "user" WHERE LOWER(username)=$1`, [
+					cred[0].toLowerCase(),
+				])
+			).length > 0
+		) {
+			res.sendStatus(430);
+			return;
+		}
+		let passwdHash = createHash("sha256").update(cred[1]).digest("base64");
+		let id = (
+			await sql(
+				`INSERT INTO "user"(username, password_hash)
+ VALUES ($1, $2) RETURNING id`,
+				[cred[0], passwdHash],
+			)
+		)[0].id;
+		const [token, expires] = await createUserSession(id, cred[0]);
+		res.cookie("session", token, { expires: expires }).sendStatus(200);
+	} catch (e) {
+		console.error(e);
+		res.sendStatus(500);
+	}
 });
 
 server.listen(process.env.PORT || 8080);
